bfbd30c1709534bb5b2e5d67536250bc2734db12
tskawada <yuta_kawada@techsword.co.jp>
2023-10-13 21:22:51 +0900

    Corrected path references in input files

--- tp.cpp
+++ tp.cpp
@@ -29,7 +29,7 @@ int normal_timelapse(Config config, Input input_att) {
     for (int i = 1; i <= config.num_of_frames; i++) {
         eta.update();
 
-        sprintf(image_name, "../data/from2/%d.%s", i, input_att.file_extension.c_str());
+        sprintf(image_name, "%s/%d.%s", config.output_file.c_str(), i, input_att.file_extension.c_str());
 
         image = imread(image_name);
         writer.write(image);
@@ -63,7 +63,7 @@ int afterimage_timelapse(Config config, Input input_att) {
         eta.update();
 
         // comparative brightness synthesis images
-        sprintf(image_name, "../data/from2/%d.%s", i, input_att.file_extension.c_str());
+        sprintf(image_name, "%s/%d.%s", config.input_folder.c_str(), i, input_att.file_extension.c_str());
         image = imread(image_name);
         
         if (i == 1) {
